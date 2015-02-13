69ea5f04bc748d51a35371192a9e10a04ae67a15
Matt Hayes <matt@mysterycommand.com>
2015-02-13 09:43:16 -0800

    4 space indentation.

--- excercises/3-events-and-state/app.js
+++ excercises/3-events-and-state/app.js
@@ -6,64 +6,64 @@ var React = require('react');
 var assign = require('react/lib/Object.assign');
 
 var DATA = [
-  { name: 'USA', description: 'Land of the free, home of the brave.' },
-  { name: 'China', description: 'Lots of concrete.' },
-  { name: 'Russia', description: 'World Cup 2018!' },
+    { name: 'USA', description: 'Land of the free, home of the brave.' },
+    { name: 'China', description: 'Lots of concrete.' },
+    { name: 'Russia', description: 'World Cup 2018!' },
 ];
 
 var App = React.createClass({
 
-  renderTabs () {
-    return this.props.countries.map((country, index) => {
-      return (
-        <div style={index === 0 ? styles.activeTab : styles.tab}>
-          {country.name}
-        </div>
-      );
-    });
-  },
+    renderTabs () {
+        return this.props.countries.map((country, index) => {
+            return (
+                <div style={index === 0 ? styles.activeTab : styles.tab}>
+                    {country.name}
+                </div>
+            );
+        });
+    },
 
-  renderPanel () {
-    var country = this.props.countries[0];
-    return (
-      <div>
-        <p>{country.description}</p>
-      </div>
-    );
-  },
+    renderPanel () {
+        var country = this.props.countries[0];
+        return (
+            <div>
+                <p>{country.description}</p>
+            </div>
+        );
+    },
 
-  render () {
-    return (
-      <div style={styles.app}>
-        <div style={styles.tabs}>
-          {this.renderTabs()}
-        </div>
-        <div style={styles.tabPanels}>
-          {this.renderPanel()}
-        </div>
-      </div>
-    );
+    render () {
+        return (
+            <div style={styles.app}>
+                <div style={styles.tabs}>
+                    {this.renderTabs()}
+                </div>
+                <div style={styles.tabPanels}>
+                    {this.renderPanel()}
+                </div>
+            </div>
+        );
 
-  }
+    }
 });
 
 var styles = {};
 
 styles.tab = {
-  display: 'inline-block',
-  padding: 10,
-  margin: 10,
-  borderBottom: '4px solid',
-  borderBottomColor: '#ccc',
-  cursor: 'pointer'
+    display: 'inline-block',
+    padding: 10,
+    margin: 10,
+    borderBottom: '4px solid',
+    borderBottomColor: '#ccc',
+    cursor: 'pointer'
 };
 
 styles.activeTab = assign({}, styles.tab, {
-  borderBottomColor: '#000'
+    borderBottomColor: '#000'
 });
 
 styles.tabPanels = {
-  padding: 10
+    padding: 10
 };
 
 React.render(<App countries={DATA}/>, document.body);
