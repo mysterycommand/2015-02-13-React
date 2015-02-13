e3ef699647200d8b2cd18d135af7ac310526e585
Matt Hayes <matt@mysterycommand.com>
2015-02-13 11:04:31 -0800

    4 space.

--- excercises/4-props-v-state/app.js
+++ excercises/4-props-v-state/app.js
@@ -16,65 +16,65 @@ var data = require('./data');
 
 var Tabs = React.createClass({
 
-  propTypes: {
-    data: React.PropTypes.array.isRequired
-  },
+    propTypes: {
+        data: React.PropTypes.array.isRequired
+    },
 
-  getInitialState () {
-    return {
-      activeTabIndex: 0
-    };
-  },
+    getInitialState () {
+        return {
+            activeTabIndex: 0
+        };
+    },
 
-  handleTabClick (activeTabIndex) {
-    this.setState({ activeTabIndex });
-  },
+    handleTabClick (activeTabIndex) {
+        this.setState({ activeTabIndex });
+    },
 
-  renderTabs () {
-    return this.props.data.map((tab, index) => {
-      var style = this.state.activeTabIndex === index ?
-        styles.activeTab : styles.tab;
-      var clickHandler = this.handleTabClick.bind(this, index);
-      return (
-        <div key={tab.name} style={style} onClick={clickHandler}>
-          {tab.name}
-        </div>
-      );
-    });
-  },
+    renderTabs () {
+        return this.props.data.map((tab, index) => {
+            var style = this.state.activeTabIndex === index ?
+                styles.activeTab : styles.tab;
+            var clickHandler = this.handleTabClick.bind(this, index);
+            return (
+                <div key={tab.name} style={style} onClick={clickHandler}>
+                    {tab.name}
+                </div>
+            );
+        });
+    },
 
-  renderPanel () {
-    var tab = this.props.data[this.state.activeTabIndex];
-    return (
-      <div>
-        <p>{tab.description}</p>
-      </div>
-    );
-  },
+    renderPanel () {
+        var tab = this.props.data[this.state.activeTabIndex];
+        return (
+            <div>
+                <p>{tab.description}</p>
+            </div>
+        );
+    },
 
-  render () {
-    return (
-      <div style={styles.app}>
-        <div style={styles.tabs}>
-          {this.renderTabs()}
-        </div>
-        <div style={styles.tabPanels}>
-          {this.renderPanel()}
-        </div>
-      </div>
-    );
-  }
+    render () {
+        return (
+            <div style={styles.app}>
+                <div style={styles.tabs}>
+                    {this.renderTabs()}
+                </div>
+                <div style={styles.tabPanels}>
+                    {this.renderPanel()}
+                </div>
+            </div>
+        );
+    }
 });
 
 var App = React.createClass({
-  render () {
-    return (
-      <div>
-        <h1>Props v. State</h1>
-        <Tabs data={this.props.tabs}/>
-      </div>
-    );
-  }
+    render () {
+        return (
+            <div>
+                <h1>Props v. State</h1>
+                <Tabs data={this.props.tabs}/>
+            </div>
+        );
+    }
 });
 
 React.render(<App tabs={data}/>, document.body);
