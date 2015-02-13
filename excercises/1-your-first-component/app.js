bccd1a5b462e5105b854b239acb49b4c9e47a1cc
Matt Hayes <matt@mysterycommand.com>
2015-02-13 07:52:57 -0800

    Task 1 complete\!

--- excercises/1-your-first-component/app.js
+++ excercises/1-your-first-component/app.js
@@ -23,11 +23,23 @@ var DATA = {
 
 var Menu = React.createClass({
   render () {
-    return null;
+    var items = this.props.items
+        .filter((item) => item.type === 'mexican')
+        .sort((a, b) => (a.name < b.name) ? -1 : ((a.name > b.name) ? 1 : 0))
+        .map((item, key) =>
+            <li id={item.id} key={item.id} className={item.type}>{item.name}</li>
+        );
+
+    return (
+        <div>
+            <h1>{this.props.title}</h1>
+            <ul>{items}</ul>
+        </div>
+    );
   }
 });
 
-React.render(<Menu/>, document.body, () => {
+React.render(<Menu {...DATA}/>, document.body, () => {
   require('./tests').run();
 });
 
